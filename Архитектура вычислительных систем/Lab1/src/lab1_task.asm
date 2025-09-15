b868c005c3f1349a41bb2d53121a1db21483f256
Anton <anton.m.lotos@gmail.com>
2025-09-15 22:54:52 +0300

    feat: update task for lab1 asm

--- Архитектура вычислительных систем/Lab1/src/lab1_task.asm
+++ Архитектура вычислительных систем/Lab1/src/lab1_task.asm
@@ -13,21 +13,31 @@ includelib 	user32.lib
 .DATA
 	a DD 30201, 30201h
 	b DB 43h, 0F3h, 0F3h, 0E5h 
-	;DF 1500
+	cc DF 1500
+	
 	;DD 1.5, 1.6, 1.9, -1.9
 	;t DQ 0E7D32A1h
 	;stra DB 16 dup(1)
 	
 	str1 db "some string 1", 0
 
-	str_fmt db "%s", 9, "%p", 9, "%p", 9, "%d", 13, 10, "%s", 9, "%p", 9, "%p", 9, "%d", 13, 10, 0
+	str_fmt db "%s", 9, "%p", 9, "%p", 9, "%d", 13, 10, "%s", 9, "%p", 9, "%p", 9, "%d", 13, 10, "%s", 9, "%p", 9, "%p", 9, "%d", 13, 10, "%s", 13, 10, 0
 	str1_name db "str1", 0
 	a_name db "a", 0
 	b_name db "b", 0
+	no_name1 db "none1", 0
+	end_name db "end of data", 0
 	out_buffer db 256 dup(0)
 	caption db "information", 0
 .CODE
 START:
+	push offset end_name 
+	;none1
+	push 6
+	push offset cc + 5 
+	push offset cc
+	push offset no_name1
+
 	;b
 	push 4
 	push offset b + 3 
@@ -39,14 +49,14 @@ START:
 	push offset a + 7 
 	push offset a 
 	push offset a_name
-	push offset str_fmt
 
+	push offset str_fmt
 	call crt_printf
 
 	push offset out_buffer	
 
 	call crt_sprintf
-	add esp, 4*9
+	add esp, 4*15
 
 	push 0
 	push offset caption 
